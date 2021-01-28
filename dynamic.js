c1ee5c7cbeccea82e69fd2e2bf884236f4f1edf6
Tajbiul-prog <71703886+Tajbiul-prog@users.noreply.github.com>
2021-01-28 23:19:06 +0600

    done

--- dynamic.js
+++ dynamic.js
@@ -1,47 +1,30 @@
 // buy phone
-const addOne = document.getElementById("addOnePhone");
-addOne.addEventListener("click", function(){
+function productHandler(isIncrease) {
     phoneQuant = document.getElementById("phoneQuantity").value;
-    phoneQuantNumb = parseFloat(phoneQuant);
-    result = phoneQuantNumb + 1;
-    document.getElementById("phoneQuantity").value = result;
-    const phoneCost = document.getElementById("phoneAmount").innerText;
-    phoneCostNumb = parseFloat(phoneCost);
-    document.getElementById("phoneAmount").innerText = phoneCostNumb + phoneCostNumb;
-
-})
-
-const minusOne = document.getElementById("minusOnePhone");
-minusOne.addEventListener("click", function(){
-    phoneQuant = document.getElementById("phoneQuantity").value;
-    phoneQuantNumb = parseFloat(phoneQuant);
-    result = phoneQuantNumb - 1;
-    if (result <= 1) {
-        result = 1;
+    phoneQuantNumb = parseInt(phoneQuant);
+    if (isIncrease == true) {
+        result = phoneQuantNumb + 1;
+    }
+    if (isIncrease == false) {
+        result = phoneQuantNumb - 1;
     }
     document.getElementById("phoneQuantity").value = result;
-})
+    const total = result * 1219;
+    document.getElementById("phoneAmount").innerText = total;
+}
+
+
 // buy phone case
-const addOneCase = document.getElementById("addOnePhoneCase");
-addOneCase.addEventListener("click", function(){
+function caseHandler(isIncrease) {
     caseQuant = document.getElementById("caseQuantity").value;
-    caseQuantNumb = parseFloat(caseQuant);
-    result = caseQuantNumb + 1;
-    document.getElementById("caseQuantity").value = result;
-    const caseCost = document.getElementById("caseAmount").innerText;
-    caseCostNumb = parseFloat(caseCost);
-    document.getElementById("caseAmount").innerText = caseCostNumb + caseCostNumb;
-
-})
-
-const minusOnePhoneCase = document.getElementById("minusOnePhoneCase");
-minusOnePhoneCase.addEventListener("click", function(){
-    caseQuant = document.getElementById("caseQuantity").value;
-    caseQuantNumb = parseFloat(caseQuant);
-    result = caseQuantNumb - 1;
-    if (result <= 1) {
-        result = 1;
+    caseQuantNumb = parseInt(caseQuant);
+    if (isIncrease == true) {
+        result = caseQuantNumb + 1;        
+    }
+    if (isIncrease == false) {
+        result = caseQuantNumb - 1;
     }
     document.getElementById("caseQuantity").value = result;
-})
-
+    const total = result * 59;
+    document.getElementById("caseAmount").innerText = total;
+}
