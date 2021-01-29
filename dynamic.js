c71b0bad77a32dae45a7594dd261116f43f1a499
Tajbiul-prog <71703886+Tajbiul-prog@users.noreply.github.com>
2021-01-29 12:55:09 +0600

    added more

--- dynamic.js
+++ dynamic.js
@@ -2,7 +2,7 @@
 function productHandler(isIncrease) {
     phoneQuant = document.getElementById("phoneQuantity").value;
     phoneQuantNumb = parseInt(phoneQuant);
-    if (isIncrease == true ) {
+    if (isIncrease == true) {
         result = phoneQuantNumb + 1;
     }
     if (isIncrease == false && phoneQuantNumb > 0) {
@@ -11,6 +11,7 @@ function productHandler(isIncrease) {
     document.getElementById("phoneQuantity").value = result;
     const total = result * 1219;
     document.getElementById("phoneAmount").innerText = total;
+    calculateTotal();
 }
 
 
@@ -19,7 +20,7 @@ function caseHandler(isIncrease) {
     caseQuant = document.getElementById("caseQuantity").value;
     caseQuantNumb = parseInt(caseQuant);
     if (isIncrease == true) {
-        result = caseQuantNumb + 1;        
+        result = caseQuantNumb + 1;
     }
     if (isIncrease == false && caseQuantNumb > 0) {
         result = caseQuantNumb - 1;
@@ -27,4 +28,18 @@ function caseHandler(isIncrease) {
     document.getElementById("caseQuantity").value = result;
     const total = result * 59;
     document.getElementById("caseAmount").innerText = total;
+    calculateTotal();
+}
+
+// calculate total
+function calculateTotal() {
+    const phoneCost = document.getElementById("phoneQuantity").value;
+    const phoneCostNumb = parseInt(phoneCost);
+
+    const caseCost = document.getElementById("caseQuantity").value;
+    const caseCostNumb = parseInt(caseCost);
+
+    const totalCost = phoneCostNumb * 1219 + caseCostNumb * 59;
+
+    document.getElementById("totalCost").innerText = "$" + totalCost;
 }
